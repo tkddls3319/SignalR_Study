ebd5f70065d5b5d03435291024d8933a6ec8d14e
SangInPark <54829486+tkddls3319@users.noreply.github.com>
2025-02-05 15:52:13 +0900

    클라이언트와 서버연결 해제 및 자동으로 재 연결

--- SignalRStudy/SecondClient/MainWindow.xaml.cs
+++ SignalRStudy/SecondClient/MainWindow.xaml.cs
@@ -23,6 +23,7 @@ namespace SecondClient
 
             hubConnection = new HubConnectionBuilder()
                 .WithUrl("https://localhost:7247/mychat")
+                .WithAutomaticReconnect()//서버랑 연결 끊겨도 자동으로 재연결
                 .Build();
 
             hubConnection.On<string, string>("receive", (message, userid) =>
@@ -30,10 +31,21 @@ namespace SecondClient
                 //멀티스레드 영역이라 디자인 스레드에서 크로스남 dispatcher은 그걸 막아줌
                 Dispatcher.Invoke(() =>
                 {
-                    chatLog.Items.Add($"{message} {userid}");
+                    chatLog.Items.Add($"{message}의 메세지 {userid}");
+                });
+            });
+
+            hubConnection.On<string>("notify", (message) =>
+            {
+                //멀티스레드 영역이라 디자인 스레드에서 크로스남 dispatcher은 그걸 막아줌
+                Dispatcher.Invoke(() =>
+                {
+                    chatLog.Items.Add($"{message}");
                 });
             });
         }
+
+
         private async void Window_Loaded(object sender, RoutedEventArgs e)
         {
             try
@@ -52,6 +64,13 @@ namespace SecondClient
         private async void btnSend_PreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
         {
            await hubConnection.InvokeAsync("Send", textBoxMessage.Text, textBoxUserId.Text);//응답을 기다림, 있음 (Task<T> 반환), 데이터 요청 (DB 조회, 연산 결과 반환 등)
+        }
+
+        private async void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            await hubConnection.InvokeAsync("Send", textBoxMessage.Text + "/"+textBoxUserId.Text, "");
+
+            await hubConnection.StopAsync();
         }
     }
 }
--- SignalRStudy/SignalRStudy/MessageHub.cs
+++ SignalRStudy/SignalRStudy/MessageHub.cs
@@ -6,7 +6,17 @@ namespace SignalRStudy
     {
         public async Task Send(string message, string userid)
         {
-            await Clients.All.SendAsync("receive", message, userid);//응답을 기다리지 않음 리턴값 없음, 순 알림, 브로드캐스트, 명령 실행
+            if (userid != null && userid.Length != 0)
+            {
+                await Clients.All.SendAsync("receive", message, userid);//응답을 기다리지 않음 리턴값 없음, 순 알림, 브로드캐스트, 명령 실행
+            }
+            else
+            {
+                if(userid != null)
+                {
+                    await Clients.Others.SendAsync("notify", "채팅방에 1명이 퇴장하였습니다.");//보낸 사용자를 제외한 모든 사용자
+                }
+            }
         }
 
     }
